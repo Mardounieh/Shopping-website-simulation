7780959e2bbb6ddee2ca9dbe01fefeff9b21081a
Amir mahdi <146704617+Mardounieh@users.noreply.github.com>
2024-03-07 12:50:26 +0330

    adds minor changes to html and javascript

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,19 +10,19 @@ const main = document.getElementById('main')
 const createProduct = (data) => {
     data.forEach(product => {
         const productDiv = document.createElement('div');
-        productDiv.classList.add('rounded', 'shadow-md','w-2/3');
+        productDiv.classList.add('rounded', 'shadow-lg','w-2/3','flex','flex-col','gap-2');
         productDiv.innerHTML = `
         <div class="">
             <img src="${product.image}" alt="" class="w-full">
         </div>
-        <div>
-            <p title="${product.title}">${product.title}</p>
-            <p>${product.price}$</p>
+        <div class="flex justify-between gap-2 px-2 font-bold">
+            <p title="${product.title}" class="font-bold">${product.title}</p>
+            <p class="rounded-md shadow p-1">${product.price}<span class="text-emerald-500">$</span></p>
         </div>
-        <div class="flex justify-between p-2">
-            <button class="fal fa-cart-circle-plus"></button>
-            <button class="fal fa-circle-ellipsis"></button>
-            <button class="fal fa-heart"></button>
+        <div class="flex justify-around p-2 text-xl">
+            <button class="fal fa-cart-circle-plus duration-100 hover:scale-125 active:scale-100"></button>
+            <button class="fal fa-circle-ellipsis duration-100 hover:scale-125 active:scale-100"></button>
+            <button class="fal fa-heart duration-100 hover:scale-125 active:scale-100"></button>
         </div>
         `
         main.appendChild(productDiv)
@@ -35,14 +35,25 @@ const getProducts = async () => {
 }
 document.addEventListener('DOMContentLoaded', getProducts)
 
+//! login and cart section
 login.addEventListener('click', () => {
     loginSection.classList.toggle('hidden');
 })
-loginSection.style.left = login.offsetLeft/1.3 + 'px'
 cart.addEventListener('click', () => {
     productCartContainer.classList.toggle('hidden')
 })
 productCartContainer.style.left = cart.offsetLeft/1.3 + 'px';
+loginSection.style.left = login.offsetLeft/1.3 + 'px'
+if(window.innerWidth <= 320) {
+    loginSection.style.left = login.offsetLeft/2 + 'px';
+    productCartContainer.style.left = cart.offsetLeft/2 + 'px';
+}
+else if(window.innerWidth > 320 && window.innerWidth <= 640) {
+    loginSection.style.left = login.offsetLeft/1.4 + 'px';
+    productCartContainer.style.left = cart.offsetLeft/1.4 + 'px';
+}
+//! login and cart section
+
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('-left-96')
     menu.classList.toggle('left-0')
